9474cd3c6a9c6264757cb828c2d01c3d40693a1d
CosmoGirl81 <flavialalbuquerque@live.com>
2025-04-09 20:39:56 -0300

    Update CartasSuperTrunfo.c
    Segue o ultimo exercicio solicitado.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,82 +1,104 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Carta {
+    string nomePais;
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+    float densidadeDemografica;
+};
+
+void exibirCarta(const Carta& carta) {
+    cout << "\nNome do país: " << carta.nomePais << endl;
+    cout << "População: " << carta.populacao << endl;
+    cout << "Área: " << carta.area << " km²" << endl;
+    cout << "PIB: " << carta.pib << " trilhões" << endl;
+    cout << "Pontos turísticos: " << carta.pontosTuristicos << endl;
+    cout << "Densidade demográfica: " << carta.densidadeDemografica << " hab/km²" << endl;
+}
+
+void compararCartas(const Carta& c1, const Carta& c2, int opcao) {
+    cout << "\nComparando " << c1.nomePais << " x " << c2.nomePais << endl;
+    switch(opcao) {
+        case 1:
+            cout << "Atributo: População" << endl;
+            cout << c1.nomePais << ": " << c1.populacao << " hab" << endl;
+            cout << c2.nomePais << ": " << c2.populacao << " hab" << endl;
+            if (c1.populacao > c2.populacao)
+                cout << "Vencedor: " << c1.nomePais << endl;
+            else if (c2.populacao > c1.populacao)
+                cout << "Vencedor: " << c2.nomePais << endl;
+            else
+                cout << "Empate!" << endl;
+            break;
+        case 2:
+            cout << "Atributo: Área" << endl;
+            cout << c1.nomePais << ": " << c1.area << " km²" << endl;
+            cout << c2.nomePais << ": " << c2.area << " km²" << endl;
+            if (c1.area > c2.area)
+                cout << "Vencedor: " << c1.nomePais << endl;
+            else if (c2.area > c1.area)
+                cout << "Vencedor: " << c2.nomePais << endl;
+            else
+                cout << "Empate!" << endl;
+            break;
+        case 3:
+            cout << "Atributo: PIB" << endl;
+            cout << c1.nomePais << ": R$ " << c1.pib << " trilhões" << endl;
+            cout << c2.nomePais << ": R$ " << c2.pib << " trilhões" << endl;
+            if (c1.pib > c2.pib)
+                cout << "Vencedor: " << c1.nomePais << endl;
+            else if (c2.pib > c1.pib)
+                cout << "Vencedor: " << c2.nomePais << endl;
+            else
+                cout << "Empate!" << endl;
+            break;
+        case 4:
+            cout << "Atributo: Pontos Turísticos" << endl;
+            cout << c1.nomePais << ": " << c1.pontosTuristicos << endl;
+            cout << c2.nomePais << ": " << c2.pontosTuristicos << endl;
+            if (c1.pontosTuristicos > c2.pontosTuristicos)
+                cout << "Vencedor: " << c1.nomePais << endl;
+            else if (c2.pontosTuristicos > c1.pontosTuristicos)
+                cout << "Vencedor: " << c2.nomePais << endl;
+            else
+                cout << "Empate!" << endl;
+            break;
+        case 5:
+            cout << "Atributo: Densidade Demográfica" << endl;
+            cout << c1.nomePais << ": " << c1.densidadeDemografica << " hab/km²" << endl;
+            cout << c2.nomePais << ": " << c2.densidadeDemografica << " hab/km²" << endl;
+            if (c1.densidadeDemografica < c2.densidadeDemografica)
+                cout << "Vencedor: " << c1.nomePais << endl;
+            else if (c2.densidadeDemografica < c1.densidadeDemografica)
+                cout << "Vencedor: " << c2.nomePais << endl;
+            else
+                cout << "Empate!" << endl;
+            break;
+        default:
+            cout << "Opção inválida. Tente novamente." << endl;
+    }
+}
 
 int main() {
-    // Declaração das variáveis para a Carta 1
-    char estadoA;
-    char A01[4];
-    char nomeCidade1[50];
-    unsigned long int populacao1;
-    float area1, pib1;
-    int pontosTuristicos1;
-    float densidade1, pibPerCapita1, superPoder1;
+    Carta carta1 = {"Brasil", 211000000, 8515767.0, 2.055, 7, 24.8};
+    Carta carta2 = {"Japão", 126300000, 377975.0, 5.081, 15, 334.2};
 
-    // Declaração das variáveis para a Carta 2
-    char estadoB;
-    char B01[4];
-    char nomeCidade2[50];
-    unsigned long int populacao2;
-    float area2, pib2;
-    int pontosTuristicos2;
-    float densidade2, pibPerCapita2, superPoder2;
+    int opcao;
 
-    // Entrada de dados para a Carta 1
-    printf("Cadastro da Carta 1:\n");
-    printf("Digite o Estado (letra de A a H): ");
-    scanf(" %c", &estadoA);
-    printf("Digite o Código da Carta (ex: A01): ");
-    scanf("%s", A01);
-    while (getchar() != '\n');
-    printf("Digite o Nome da Cidade: ");
-    fgets(nomeCidade1, sizeof(nomeCidade1), stdin);
-    nomeCidade1[strcspn(nomeCidade1, "\n")] = 0;
-    printf("Digite a População: ");
-    scanf("%lu", &populacao1);
-    printf("Digite a Área (em km²): ");
-    scanf("%f", &area1);
-    printf("Digite o PIB (em bilhões de reais): ");
-    scanf("%f", &pib1);
-    printf("Digite o Número de Pontos Turísticos: ");
-    scanf("%d", &pontosTuristicos1);
+    cout << "\n===== Menu de Comparação de Cartas =====" << endl;
+    cout << "1. População" << endl;
+    cout << "2. Área" << endl;
+    cout << "3. PIB" << endl;
+    cout << "4. Pontos Turísticos" << endl;
+    cout << "5. Densidade Demográfica" << endl;
+    cout << "Escolha um atributo para comparar (1-5): ";
+    cin >> opcao;
 
-    // Entrada de dados para a Carta 2
-    printf("\nCadastro da Carta 2:\n");
-    printf("Digite o Estado (letra de A a H): ");
-    scanf(" %c", &estadoB);
-    printf("Digite o Código da Carta (ex: A01): ");
-    scanf("%s", B01);
-    while (getchar() != '\n');
-    printf("Digite o Nome da Cidade: ");
-    fgets(nomeCidade2, sizeof(nomeCidade2), stdin);
-    nomeCidade2[strcspn(nomeCidade2, "\n")] = 0;
-    printf("Digite a População: ");
-    scanf("%lu", &populacao2);
-    printf("Digite a Área (em km²): ");
-    scanf("%f", &area2);
-    printf("Digite o PIB (em bilhões de reais): ");
-    scanf("%f", &pib2);
-    printf("Digite o Número de Pontos Turísticos: ");
-    scanf("%d", &pontosTuristicos2);
-
-    // Cálculos
-    densidade1 = populacao1 / area1;
-    pibPerCapita1 = (pib1 * 1000000000) / populacao1;
-    densidade2 = populacao2 / area2;
-    pibPerCapita2 = (pib2 * 1000000000) / populacao2;
-
-    // Cálculo do Super Poder
-    superPoder1 = populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1 / densidade1);
-    superPoder2 = populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1 / densidade2);
-
-    // Comparação das cartas
-    printf("\nComparação de Cartas:\n");
-    printf("População: Carta %d venceu (%d)\n", populacao1 > populacao2 ? 1 : 2, populacao1 > populacao2);
-    printf("Área: Carta %d venceu (%d)\n", area1 > area2 ? 1 : 2, area1 > area2);
-    printf("PIB: Carta %d venceu (%d)\n", pib1 > pib2 ? 1 : 2, pib1 > pib2);
-    printf("Pontos Turísticos: Carta %d venceu (%d)\n", pontosTuristicos1 > pontosTuristicos2 ? 1 : 2, pontosTuristicos1 > pontosTuristicos2);
-    printf("Densidade Populacional: Carta %d venceu (%d)\n", densidade1 < densidade2 ? 1 : 2, densidade1 < densidade2);
-    printf("PIB per Capita: Carta %d venceu (%d)\n", pibPerCapita1 > pibPerCapita2 ? 1 : 2, pibPerCapita1 > pibPerCapita2);
-    printf("Super Poder: Carta %d venceu (%d)\n", superPoder1 > superPoder2 ? 1 : 2, superPoder1 > superPoder2);
+    compararCartas(carta1, carta2, opcao);
 
     return 0;
 }
